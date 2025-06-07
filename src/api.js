4883c2fb0db4cc82fb277e1bc6b64cc4e4d451ca
shreyash-mokashi <132278143+shreyash-mokashi@users.noreply.github.com>
2025-06-07 16:42:48 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://v-task-backend.onrender.com",
+  baseURL: "https://v-task-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
